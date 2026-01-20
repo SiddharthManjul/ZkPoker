4a3b252589c5d750527f194c064cdafcef3ca9f6
SiddharthManjul <manjulsiddharth09@gmail.com>
2026-01-20 15:59:29 +0530

    instructions module

--- contracts/programs/contracts/src/instructions/mod.rs
+++ contracts/programs/contracts/src/instructions/mod.rs
@@ -4,6 +4,7 @@ pub mod hand;
 pub mod betting;
 pub mod reveal;
 pub mod showdown;
+pub mod proof_buffer;
 
 pub use admin::*;
 pub use table::*;
@@ -11,3 +12,4 @@ pub use hand::*;
 pub use betting::*;
 pub use reveal::*;
 pub use showdown::*;
+pub use proof_buffer::*;
